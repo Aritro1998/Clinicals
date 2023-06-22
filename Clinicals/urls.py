3df9ef8b86fe1f667842191fe7e99d6185cc38cc
Aritro Chowdhury <chowdhury.aritro1998@gmail.com>
2023-06-22 11:54:54 +0530

    Create Delete functionality

--- Clinicals/urls.py
+++ Clinicals/urls.py
@@ -21,5 +21,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.PatientListView.as_view(), name='index'),
     path('create/',views.PatientCreateView.as_view()),
-    path('update/<int:pk>',views.PatientUpdateView.as_view())
+    path('update/<int:pk>/',views.PatientUpdateView.as_view()),
+    path('delete/<int:pk>/',views.PatientDeleteView.as_view())
 ]
