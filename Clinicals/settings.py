5a355d3be98886ba8616ed3c9f146988c856d01c
Aritro Chowdhury <chowdhury.aritro1998@gmail.com>
2023-06-22 09:36:47 +0530

    Make migrations, setup urls and make corrections.

--- Clinicals/settings.py
+++ Clinicals/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -37,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'clinicalsApp'
 ]
 
 MIDDLEWARE = [
@@ -54,7 +56,7 @@ ROOT_URLCONF = 'Clinicals.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
+        'DIRS': [os.path.join(BASE_DIR, 'Clinicals/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
